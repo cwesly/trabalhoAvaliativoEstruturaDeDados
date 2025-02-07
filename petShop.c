bc29fcefb650ea150f5efed32f9ab820e72ff0be
cwesly <cauawesly@outlook.com>
2025-02-07 20:36:58 -0300

    feat: create switch case menu

--- petShop.c
+++ petShop.c
@@ -2,10 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct Animal{
+typedef struct Animal
+{
     char nome[50];
     char tutor[50];
     char servico[50];
     char status[50];
     struct Animal proximo;
 } Animal;
+
+int main()
+{
+    int opcao = 1;
+    do
+    {
+        printf("\nBem-vindo ao Pet Shop Mundo Cão!\n");
+        printf("Escolha uma opção:\n");
+        printf("1 - Cadastrar animal.\n");
+        printf("Opção: ");
+        scanf("%d", opcao);
+
+        switch (opcao)
+        {
+        case 1:
+            cadastrarAnimal();
+            break;
+
+        case 9:
+            printf("Saindo...");
+            break;
+
+        default:
+            printf("Opção inválida!");
+            break;
+        }
+    } while (opcao != 9);
+}
