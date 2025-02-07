312e1669296e28bf028badb9cd3b88f3e20fb2a9
cwesly <cauawesly@outlook.com>
2025-02-07 20:35:54 -0300

    feat: add Animal struct

--- petShop.c
+++ petShop.c
@@ -2,3 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct Animal{
+    char nome[50];
+    char tutor[50];
+    char servico[50];
+    char status[50];
+    struct Animal proximo;
+} Animal;
